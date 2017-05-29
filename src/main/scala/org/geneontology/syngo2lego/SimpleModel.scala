914299a4a9072ae3efc1487575ae9ba100db1931
David Osumi-Sutherland <dosumis@gmail.com>
2017-05-29 18:57:22 +0100

    Fixing annotation: All inds annotated. Each edge gets new annotation inds.

--- src/main/scala/org/geneontology/syngo2lego/SimpleModel.scala
+++ src/main/scala/org/geneontology/syngo2lego/SimpleModel.scala
@@ -11,6 +11,8 @@ import scala.language.postfixOps
 import scala.collection.mutable.ArrayBuffer
 import scala.collection.JavaConversions._
 import collection.JavaConverters._
+import java.util.Date
+import java.text.SimpleDateFormat
 
 class SimpleModel (val model_ns: String, var ont : BrainScowl, val jmodel : Json, val go: BrainScowl) {
   // Passing rather too many vars.  Deeper class model with some 
@@ -30,10 +32,29 @@ class SimpleModel (val model_ns: String, var ont : BrainScowl, val jmodel : Json
   // Lookup needed to translate identifiers from JSON
   val OP_lookup = Map( "part_of" -> part_of, 
       "occurs_in" ->  occurs_in)  // More concise way to do this?
+      
+  val primary_class = Class(obo_ns + this.jmodel.goTerm.as[String].replace(":", "_"))
+      
+  // Annotations // Global 'cos they go on everything.
+      
+  val dc_contributor = AnnotationProperty("http://purl.org/dc/elements/1.1/contributor")
+//  val contributor = IRI.create(synGO_ns + jmodel.username.as[String])
+  val contributor = synGO_ns + jmodel.username.as[String]
+  val source = "PMID:" + jmodel.pmid.as[String]
+  val evidence = AnnotationProperty("http://geneontology.org/lego/evidence")    
+
+  this.ont.annotateOntology(Annotation (dc_contributor, contributor))
+  val dc_date = AnnotationProperty("http://purl.org/dc/elements/1.1/date")
   
+  // Temporary expedient
+  def date(): String = {
+    val now = new Date()
+    val ft = new SimpleDateFormat("yyyy-MM-dd")
+    return ft.format(now)
+  }
+ 
   // methods for generating and adding new inds.  All return ind.
 
-  val primary_class = Class(obo_ns + this.jmodel.goTerm.as[String].replace(":", "_")) //  Add in call to UUID gen 
   
   // Lookup OBO namespace of primary term.  Use Ontology to do this.
   // Do NOT use goDomain from the JSON as MFs are mislabelled as BPs:
@@ -45,7 +66,10 @@ class SimpleModel (val model_ns: String, var ont : BrainScowl, val jmodel : Json
 
   
   def new_ind() : OWLNamedIndividual = {
-    return NamedIndividual(this.model_ns + java.util.UUID.randomUUID.toString)
+    val i = NamedIndividual(this.model_ns + java.util.UUID.randomUUID.toString)
+    this.ont.add_axiom(i Annotation (dc_contributor, contributor)) // Also needs date.
+    this.ont.add_axiom(i Annotation (dc_date, date)) // Also needs date.    
+    return i
   }
   
   // The following could be made into a generic method that gets passed an ind gen fn
@@ -91,31 +115,22 @@ class SimpleModel (val model_ns: String, var ont : BrainScowl, val jmodel : Json
   def gen_annotations(evj: Json): Set[OWLAnnotation] = {
     // Generate a set of individuals to be attached to edges as evidence
     // Also annotates model with contributor
-    val dc_source = AnnotationProperty("http://purl.org/dc/elements/1.1/source")
-    val dc_contributor = AnnotationProperty("http://purl.org/dc/elements/1.1/contributor")
-//  val contributor = IRI.create(synGO_ns + jmodel.username.as[String])
-    val contributor = synGO_ns + jmodel.username.as[String]
-    val source = "PMID:" + jmodel.pmid.as[String]
-    val evidence = AnnotationProperty("http://geneontology.org/lego/evidence")    
-    this.ont.annotateOntology(Annotation (dc_contributor, contributor))
     // Should probably add these to the ontology too - but feels like wrong place to do it.
   ///  this.ont.add_axiom(ont.ontology Annotation(dc_source, source))
+     val dc_source = AnnotationProperty("http://purl.org/dc/elements/1.1/source")
      var out = Set[OWLAnnotation]()  
        for ((k,v) <- jmodel.evidence.as[Map[String, Json]]) {
-       // How to interpolate object names here !?!:
+         // TODO annotated inds with syngo evidence codes. Needs extension to JSON.
          for (eco <- v.as[List[List[String]]]) {  // Temporary fix for https://github.com/geneontology/synapse/issues/130
-           val ann = new_typed_ind(obo_ns + eco(0).replace(":", "_")) // Temporary fix for https://github.com/geneontology/synapse/issues/130
-           this.ont.add_axiom(ann Annotation (dc_source, source)) 
-           this.ont.add_axiom(ann Annotation (dc_contributor, contributor)) 
+           val ann = new_typed_ind(obo_ns + eco(0).replace(":", "_")) // (0) is temporary fix for https://github.com/geneontology/synapse/issues/130
+           this.ont.add_axiom(ann Annotation (dc_source, source))
            out += Annotation(evidence, ann)
          }
        }
      return out
     }
   
-  
-  val annotations = gen_annotations(jmodel.evidence)
-  
+    
  // primary entity   
   // Add extensions
   
@@ -126,15 +141,18 @@ class SimpleModel (val model_ns: String, var ont : BrainScowl, val jmodel : Json
       val primary_ind = new_primary_ind()
    // ObjectPropertyAssertion(annotations, loves, Aya, Freddy)     // For ref
     if (primary_aspect == "cellular_component") {
+      val annotations = gen_annotations(jmodel.evidence)
       val mfi = new_mfi()
       this.ont.add_axiom(mfi Fact (enabled_by, gp))
       this.ont.add_axiom(ObjectPropertyAssertion(annotations, occurs_in, mfi, primary_ind))
     }
     if (primary_aspect == "molecular_function") {
+      val annotations = gen_annotations(jmodel.evidence)
       this.ont.add_axiom(ObjectPropertyAssertion(annotations, enabled_by, primary_ind, gp))       
     }  
   //    this.ont.add_axiom(ObjectPropertyAssertion(annotations, gp, occurs_in, mfi)  Ask!
     if (primary_aspect == "biological_process") {
+      val annotations = gen_annotations(jmodel.evidence)
       val mfi = new_mfi()
       this.ont.add_axiom(mfi Fact (enabled_by, gp))
       this.ont.add_axiom(ObjectPropertyAssertion(annotations, part_of, mfi, primary_ind))
@@ -146,7 +164,9 @@ class SimpleModel (val model_ns: String, var ont : BrainScowl, val jmodel : Json
 // fu["SynGO"][2]['models'][0]['extensions']
 // [{'part_of': ['UBERON:0001869']}, {'part_of': ['UBERON:0002421']}]
 // [{'occurs_in': ['UBERON:0000955', 'Glutamatergic']}, {'occurs_in': ['UBERON:0000955', 'GABAergic']}]
-// This second example suggests two separate annotations!
+  
+// This second example =>  two separate annotations
+// Note - could potentially be improved by assuming Uberon part_of GO.
   
   
   // if none create one annotation, other wise create same number as length.
